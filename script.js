3434fb107e8d6c8bcfb40c6bf8ff0e1edd26ffcf
ansh-2004 <ansh1295.be22@chitkara.edu.in>
2025-04-11 03:48:47 +0530

    try for publishing as PWA

--- script.js
+++ script.js
@@ -254,5 +254,14 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
   
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js").then(
+      (reg) => console.log("Service Worker registered", reg),
+      (err) => console.log("Service Worker registration failed", err)
+    );
+  });
+}
+
 
 window.onload = loadCredentials;
