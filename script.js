c0a0922dfc3d973378b43f54d35bcbf9cd86d433
ansh-2004 <ansh1295.be22@chitkara.edu.in>
2025-04-11 02:16:40 +0530

    adding deployed backend api

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API = 'http://localhost:5000';
+const API = 'https://password-manager-server-v5al.onrender.com';
 
 document.getElementById('credentialForm').addEventListener('submit', async (e) => {
   e.preventDefault();
